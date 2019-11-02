3a8fcc03c7c1d0bd2727f9049b973bfe394db9c7
Laurence <liuxy6@gmail.com>
2019-11-02 14:37:53 -0400

    Add comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 
+// x: string
+// n: n shares
+// m: at least m shares required to restore x
+// ->: array of hex strings
 function split(x, n, m) {
     return secrets.share(secrets.str2hex(x), n, m);
 }
 
+// shares: array of hex strings
+// ->: x
 function combine(shares) {
     return secrets.hex2str(secrets.combine(shares));
 }
