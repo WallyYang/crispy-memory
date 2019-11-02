bc8b96540b85ac749caa64cb3cb2f0c1201dea8a
Laurence <liuxy6@gmail.com>
2019-11-02 14:25:44 -0400

    Add secrets

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 
-function main() {
-    
+function split(x, n, m) {
+    return secrets.share(secrets.str2hex(x), n, m);
+}
+
+function combine(shares) {
+    return secrets.hex2str(secrets.combine(shares));
 }
